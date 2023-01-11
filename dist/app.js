0e329a5cc65d86b164c17b2c0a2409ee4bec4514
Decagon <uchecharles223@gmail.com>
2023-01-11 17:03:15 +0100

    did the database connection

--- dist/app.js
+++ dist/app.js
@@ -4,7 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const auth_1 = __importDefault(require("./routes/auth"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+mongoose_1.default.set('strictQuery', false);
+mongoose_1.default.connect(process.env.CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error.message));
+app.use("/auth", auth_1.default);
 app.listen(5000, () => {
     console.log('Server running on port 5000');
 });
+exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import express, {Request, Response} from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoute from './routes/auth';
+dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+  
+mongoose.set('strictQuery', false);
+mongoose.connect(process.env.CONNECT_URL!, <MongoDBOptions>{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch((error: Error) => console.log(error.message));
+
+app.use("/auth", authRoute)
 
 app.listen(5000, () => {
     console.log('Server running on port 5000');
 })
+
+export default app;
